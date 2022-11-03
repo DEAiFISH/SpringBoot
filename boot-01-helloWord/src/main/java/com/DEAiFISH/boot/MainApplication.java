13f58a54793de175805382b4674f1ae4a2d577b5
DEAIFISH <1277676309@qq.com>
2022-11-03 22:59:01 +0800

    2022/11/03

--- boot-01-helloWord/src/main/java/com/DEAiFISH/boot/MainApplication.java
+++ boot-01-helloWord/src/main/java/com/DEAiFISH/boot/MainApplication.java
@@ -4,7 +4,6 @@ package com.DEAiFISH.boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
 
 /**
  * 主程序类
