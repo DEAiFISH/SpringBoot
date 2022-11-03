bf3a50364b8cf734f11745684713ca0f02cece3d
DEAIFISH <1277676309@qq.com>
2022-11-03 14:41:49 +0800

    2022/11/03

--- boot-01-helloWord/src/main/java/com/DEAiFISH/boot/MainApplication.java
+++ boot-01-helloWord/src/main/java/com/DEAiFISH/boot/MainApplication.java
@@ -3,15 +3,20 @@ package com.DEAiFISH.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * 主程序类
  *
  * @SpringBootApplication：这是一个SpringBoot应用
  */
-@SpringBootApplication
+//@SpringBootApplication(scanBasePackages = "com.DEAiFISH.boot"/*设置扫描包位置，默认为main方法所在包*/)
+@SpringBootApplication()
 public class MainApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MainApplication.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(MainApplication.class, args);
+
+        System.out.println(run.getBean("晁祥翔"));
     }
 }
---
+++ boot-01-helloWord/src/main/java/com/DEAiFISH/boot/config/MyConfig.java
@@ -0,0 +1,13 @@
+package com.DEAiFISH.boot.config;
+
+import com.DEAiFISH.boot.bean.User;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration(proxyBeanMethods = true/*是否开启代理*/)
+public class MyConfig {
+    @Bean("晁祥翔"/*bean 默认名字*/)
+    public User user(){
+        return new User("cxx",20,"男");
+    }
+}
