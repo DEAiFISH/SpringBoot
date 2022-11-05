3bac1f1b057f45b8939176acf374f86b00b8da87
DEAIFISH <1277676309@qq.com>
2022-11-05 08:51:21 +0800

    2022/11/05

--- boot-01-helloWord/src/main/java/com/DEAiFISH/boot/controller/HelloWordController.java
+++ boot-01-helloWord/src/main/java/com/DEAiFISH/boot/controller/HelloWordController.java
@@ -1,12 +1,22 @@
 package com.DEAiFISH.boot.controller;
 
+import com.DEAiFISH.boot.bean.Car;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWordController {
+    @Autowired
+    Car car;
+
+    @RequestMapping("/car")
+    public Car car() {
+        return car;
+    }
+
     @RequestMapping("/hello")
-    public String handle01(){
+    public String handle01() {
         return "Hello, Spring Boot 2!";
     }
 }
