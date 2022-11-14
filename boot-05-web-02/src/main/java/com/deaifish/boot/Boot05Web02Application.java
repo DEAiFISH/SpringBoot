4312c309605954fbf7e6b1217d760899037235ee
DEAIFISH <1277676309@qq.com>
2022-11-14 22:12:47 +0800

    2022/11/14

---
+++ boot-05-web-02/src/main/java/com/deaifish/boot/Boot05Web02Application.java
@@ -0,0 +1,13 @@
+package com.deaifish.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot05Web02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot05Web02Application.class, args);
+    }
+
+}
